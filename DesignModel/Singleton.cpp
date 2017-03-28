65290a68174635749f794fe5a1c4cdfd71237bcf
dongruibin <2423903781@qq.com>
2017-03-28 23:40:02 +0800

    1、设计模式学习

---
+++ DesignModel/Singleton.cpp
@@ -0,0 +1,29 @@
+#include "Singleton.h"
+#include <iostream>
+
+using namespace std;
+//说明：单例模式的使用
+
+
+#if 1
+class singleton
+{
+
+};
+Singleton* getInstance()
+{
+	if(instance==NULL)
+	{
+
+	}
+}
+
+int main(int argc,char** argv)
+{
+
+	return 0;
+}
+#endif
+
+
+
