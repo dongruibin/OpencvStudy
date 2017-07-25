97f58487d3f4664c3612a971930995f7114f4633
dongruibin <2423903781@qq.com>
2017-07-25 22:55:17 +0800

    1、提交链表学习插入节点，没有看明白

--- LinkList/LinkListTest.cpp
+++ LinkList/LinkListTest.cpp
@@ -1,9 +1,14 @@
+//学习参考链接：http://blog.csdn.net/kevin_zhai/article/details/50494020
 #include "include.h"
 #include <iostream>
 
 using namespace std;
 //本文件之前有一版在ubuntu上创建的，结构体编译始终显示
 //缺少一个"}"，实际上并没有缺少，具体原因现在还没有发现。
+/*
+链表中，每个节点包括存储数据元素的数据域，另一个是指向
+下一个节点地址的指针域
+*/
 struct Info
 {
 	string name;
@@ -28,13 +33,43 @@ public:
 	void Print();//打印链表
 	~LinkList();
 private:
-	Node* head;//指向节点的指针
+	Node* head;//链表里面的头？
 	int length;//整个链表的长度
 };
 //@param 
 void LinkList::Insert(Info val,int pos)
 {
 	cout<<"LinkList Insert"<<endl;
+	if(pos<0)
+	{
+		cout<<"pos must be greater than zero"<<endl;
+		return;
+	}
+	int index=1;
+	Node *temp=head;//指向节点的指针
+	Node *node=new Node(val);//调用有参构造，构造节点，也就是将要
+	//插入数据变成节点形式
+	if(pos==0)
+	{
+		node->next=temp;//这里还不是赋值，是让指针指向头
+		head=node;
+		length++;
+		return;
+	}
+	while(temp!=NULL&&index<pos)
+	{
+		temp=temp->next;
+		index++;
+
+	}
+	if(temp==NULL)
+	{
+		cout<<"Insert failed"<<endl;
+		return;
+	}
+	node->next=temp->next;
+	temp->next=node;
+	length++;
 }
 
 #ifdef USE_LinkList
